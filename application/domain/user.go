0e5031bd672b8056170acc5a0f0168666a4960bd
Miguel Brito <miguel.brito@levva.io>
2024-01-21 00:59:44 -0300

    refactor: changing json tag to bson tag on domain

--- application/domain/user.go
+++ application/domain/user.go
@@ -1,9 +1,9 @@
 package domain
 
 type User struct {
-	Nickname    string   `json:"nickname"`
-	Password    string   `json:"password"`
-	Gender      string   `json:"gender"`
-	PhoneNumber string   `json:"phoneNumber"`
-	Interests   []string `json:"interests"`
+	Nickname    string   `bson:"nickname"`
+	Password    string   `bson:"password"`
+	Gender      string   `bson:"gender"`
+	PhoneNumber string   `bson:"phoneNumber"`
+	Interests   []string `bson:"interests"`
 }
